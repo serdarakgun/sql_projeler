e9befb30182990f2f5ab7a15eb0a68bc09a6d812
serdarakgun <serdarakgun1034@gmail.com>
2023-03-27 23:06:19 +0300

    basics
    patika.dev sitesi için hazırlanmıştır

---
+++ ödev1.sql
@@ -0,0 +1,10 @@
+
+--select title,description from film;
+
+--select * from film where length>60 and length<75;
+
+--select * from film where rental_rate=0.99 and (replacement_cost=12.99 or replacement_cost=28.99);
+
+--select last_name from customer where first_name='Mary';
+
+--select * from film where length<50 and Not (rental_rate=2.99 or rental_rate=4.99);
