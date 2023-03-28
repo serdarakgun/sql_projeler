dec4b4face0f86af87b94f39ce4afceca15093b9
serdarakgun <serdarakgun1034@gmail.com>
2023-03-28 18:38:16 +0300

    basics4
    patika.dev sitesi için hazırlanmıştır

---
+++ ödev4.sql
@@ -0,0 +1,9 @@
+--select distinct replacement_cost from film ;
+
+--select count(distinct replacement_cost) from film ;
+
+--select count(*) from film where title like 'T%' and rating='G';
+
+--select count(*) from country where length(country)=5;
+
+--select count(*) from city where city ilike '%r';
