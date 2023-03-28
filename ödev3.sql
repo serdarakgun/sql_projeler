076766fe195a74a8bd5a31035983e69c3b5bae4b
serdarakgun <serdarakgun1034@gmail.com>
2023-03-28 18:22:37 +0300

    basics3
    patika.dev sitesi için hazırlanmıştır

---
+++ ödev3.sql
@@ -0,0 +1,7 @@
+--select country from country where country like 'A%a';
+
+--select country from country where country like '%n' and length(country)>5;
+
+--select title from film where title ilike '%t%t%t%t%';
+
+--select * from film where title like 'C%' and length>90 and rental_rate=2.99;
