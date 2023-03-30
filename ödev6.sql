d954478cc4f7e60968567cced0b3de9cc1095c6f
serdarakgun <serdarakgun1034@gmail.com>
2023-03-30 13:04:05 +0300

    basics6
    patika.dev sitesi için hazırlandı

---
+++ ödev6.sql
@@ -0,0 +1,7 @@
+--select avg(rental_rate) from film;
+
+--select count(title) from film where title like 'C%';
+
+--select max(length) from film where rental_rate=0.99;
+
+--select count(distinct replacement_cost) from film where length>150;
