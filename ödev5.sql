3f26f0e193c975d3627c6e72f73b345c53c91876
serdarakgun <serdarakgun1034@gmail.com>
2023-03-30 12:57:24 +0300

    basics5
    patika.dev sitesi için hazırlandı

---
+++ ödev5.sql
@@ -0,0 +1,5 @@
+--select title from film where title like '%n' order by length desc limit 5;
+
+--select * from film where title like '%n' order by length asc offset 5 limit 5;
+
+--select * from customer where store_id=1 order by last_name desc limit 4;
